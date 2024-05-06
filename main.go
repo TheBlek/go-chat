f41ad51db9f10c6d096bd3941bd58f467007d710
Georgiy Kuklin <egorka99966@gmail.com>
2024-05-06 13:59:46 +0700

    First test

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net"
+    "fmt"
+)
+
+func main() {
+    conn, err := net.ListenPacket("udp4", ":8829")
+    if err != nil {
+        panic(err)
+    }
+    defer conn.Close()
+
+    broadcast_addr, err := net.ResolveUDPAddr("udp4", "127.0.0.255:8829")
+    if err != nil {
+        panic(err)
+    }
+    conn.WriteTo([]byte("Hellope"), broadcast_addr)
+
+    data := [2048]byte{0}
+    size, from, err := conn.ReadFrom(data[:])
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Received %v from %v\n", string(data[:size]), from)
+}
